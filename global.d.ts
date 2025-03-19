3f9bfeee3f02d824c7abfec4a249d090e474449a
Azfar <azfarrazzaq23@gmail.com>
2025-03-19 11:53:51 +0500

    Add global type declarations for table-library modules and update tsconfig

---
+++ global.d.ts
@@ -0,0 +1,15 @@
+// global.d.ts
+
+declare module '@table-library/react-table-library/theme.js' {
+    // Re-export everything from the original module
+    export * from '@table-library/react-table-library/theme';
+  }
+  
+  declare module '@table-library/react-table-library/baseline.js' {
+    export * from '@table-library/react-table-library/baseline';
+  }
+  
+  declare module '@table-library/react-table-library/compact.js' {
+    export * from '@table-library/react-table-library/compact';
+  }
+  

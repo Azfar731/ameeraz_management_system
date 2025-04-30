c5437e7861ccfbb8f8eb3e437b7da954eb747a1e
Azfar <azfarrazzaq23@gmail.com>
2025-04-30 11:58:47 +0500

    added styling to  un-authorized page

--- app/routes/un-authorized.tsx
+++ app/routes/un-authorized.tsx
@@ -2,9 +2,11 @@ import { Link } from "@remix-run/react";
 
 export default function UnAuthorized() {
   return (
-    <>
-      <h2>You are not authorized to access this page</h2>
-      <Link to="/">Go Back to Home</Link>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h2 className=" text-3xl">You are not authorized to access this page</h2>
+      <div className="px-4 py-2 mt-4 bg-red-500 rounded shadow-md text-white">
+        <Link to="/" >Go Back to Home</Link>
+      </div>
+    </div>
   );
 }
